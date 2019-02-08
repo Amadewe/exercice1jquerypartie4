82b9506100d810bcd6a35a54854175004ceb8107
Amadewe <dewevre.amandine@gmail.com>
2019-02-08 15:36:24 +0100

    Nouvelle version

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -3,34 +3,46 @@ $(function(){
   var counterLooses = 0;
 
 $('#button').click(function(){
-  // la var ramdom permet de choisir un élément au hasard pour une liste de 3
-  // il y a une version possible quand nous ne connaissons pas le nombre d'élement dans la liste
-  // fonction ramdom doit etre dans ma fonction click pour gérer aléatoirement plusieurs réponses ...
+  // la var ramdom permet de choisir un élément au hasard pour une liste de 3 (3 car nos élèments sont 0,1,2)
   var computer = Math.floor(Math.random()*3);
   // on utilise la méthode array sur la liste de nos éléments
-  var array = ['pierre', 'feuille', 'ciseaux'];
-  var choiseComputer = array[computer];
-  // maintenant on crée un tableau pierre feuille ciseaux pour l'utilisateur
-  var player = $('option:selected').val();
-  var choisePlayer = array[player];
-// var du pourcentage:
+  // var array = ['pierre', 'feuille', 'ciseaux'];
+  // var choiceComputer = array[computer];
+  // maintenant on crée récupère les valeurs du player
+  var player = $('.list option:selected').val();
+  // var choicePlayer = array[player];
 
-// Bonus : Ajouter un compteur de victoires et de défaites et afficher le pourcentage de victoire contre l'ordinateur.
+  // Bonus : Ajouter un compteur de victoires et de défaites et afficher le pourcentage de victoire contre l'ordinateur.
+console.log(computer);
 
-console.log(choiseComputer);
- if ((choiseComputer) == (choisePlayer)){
-   alert ('Match nul');
- } else if ((choiseComputer == 'pierre' && choisePlayer == 'feuille') || (choiseComputer == 'feuille' && choisePlayer == 'ciseaux') || (choiseComputer == 'ciseaux' && choisePlayer == 'pierre')) {
+if (player == computer){
+  alert ('Match nul');
+} else if ((computer == 0 && player == 1) || (computer == 1 && player == 2) || (computer == 2 && player == 0)) {
 alert ('Vous avez gagné');
-counterVictories ++
-} else if ((choiseComputer == 'feuille' && choisePlayer == 'pierre') || (choiseComputer == 'ciseaux' && choisePlayer == 'feuille') || (choiseComputer == 'pierre' && choisePlayer == 'ciseaux')) {
+counterVictories ++;
+} else {
 alert ('Vous avez perdu');
-counterLooses ++
+counterLooses ++;
 }
 alert('score joueur: ' + counterVictories + ' score ordinateur: ' + counterLooses);
+
+
+
+
+//  if ((choiceComputer) == (choicePlayer)){
+//    alert ('Match nul');
+//  } else if ((choiceComputer == 'pierre' && choicePlayer == 'feuille') || (choiceComputer == 'feuille' && choicePlayer == 'ciseaux') || (choiceComputer == 'ciseaux' && choicePlayer == 'pierre')) {
+// alert ('Vous avez gagné');
+// counterVictories ++
+// } else if ((choiceComputer == 'feuille' && choicePlayer == 'pierre') || (choiceComputer == 'ciseaux' && choicePlayer == 'feuille') || (choiceComputer == 'pierre' && choicePlayer == 'ciseaux')) {
+// alert ('Vous avez perdu');
+// counterLooses ++
+// }
+// alert('score joueur: ' + counterVictories + ' score ordinateur: ' + counterLooses);
+// on crée une var pour le pourcentage de victoire
 var percentage = Math.round((counterVictories/(counterVictories+counterLooses))*100);
 
-console.log(percentage);
+// console.log(percentage);
 alert('Votre pourcentage de victoire est de: ' + percentage + '%');
 });
 });
