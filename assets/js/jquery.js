eaa1b3cd57bddf6f1b8253976079e5e2840ffff2
Amadewe <dewevre.amandine@gmail.com>
2019-02-09 10:31:33 +0100

    Correction

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,48 +1,32 @@
-$(function(){
-  var counterVictories = 0;
-  var counterLooses = 0;
-
-$('#button').click(function(){
-  // la var ramdom permet de choisir un élément au hasard pour une liste de 3 (3 car nos élèments sont 0,1,2)
-  var computer = Math.floor(Math.random()*3);
-  // on utilise la méthode array sur la liste de nos éléments
-  // var array = ['pierre', 'feuille', 'ciseaux'];
-  // var choiceComputer = array[computer];
-  // maintenant on crée récupère les valeurs du player
-  var player = $('.list option:selected').val();
-  // var choicePlayer = array[player];
-
-  // Bonus : Ajouter un compteur de victoires et de défaites et afficher le pourcentage de victoire contre l'ordinateur.
-console.log(computer);
-
-if (player == computer){
-  alert ('Match nul');
-} else if ((computer == 0 && player == 1) || (computer == 1 && player == 2) || (computer == 2 && player == 0)) {
-alert ('Vous avez gagné');
-counterVictories ++;
-} else {
-alert ('Vous avez perdu');
-counterLooses ++;
-}
-alert('score joueur: ' + counterVictories + ' score ordinateur: ' + counterLooses);
-
-
-
-
-//  if ((choiceComputer) == (choicePlayer)){
-//    alert ('Match nul');
-//  } else if ((choiceComputer == 'pierre' && choicePlayer == 'feuille') || (choiceComputer == 'feuille' && choicePlayer == 'ciseaux') || (choiceComputer == 'ciseaux' && choicePlayer == 'pierre')) {
-// alert ('Vous avez gagné');
-// counterVictories ++
-// } else if ((choiceComputer == 'feuille' && choicePlayer == 'pierre') || (choiceComputer == 'ciseaux' && choicePlayer == 'feuille') || (choiceComputer == 'pierre' && choicePlayer == 'ciseaux')) {
-// alert ('Vous avez perdu');
-// counterLooses ++
-// }
-// alert('score joueur: ' + counterVictories + ' score ordinateur: ' + counterLooses);
-// on crée une var pour le pourcentage de victoire
-var percentage = Math.round((counterVictories/(counterVictories+counterLooses))*100);
-
-// console.log(percentage);
-alert('Votre pourcentage de victoire est de: ' + percentage + '%');
-});
+$(document).ready(function() {
+  var victory = 0;
+  var defeat = 0;
+  var even = 0;
+  // Je lance ma fonction quand je click sur le bouton
+  $('#shiFuMi').click(function(){
+    // Je stock dans une variable un tableau(Array)
+    var choices = ['Feuille','Ciseaux','Pierre'];
+    //Je génère le choix de l'ordinateur
+    var computerChoice = Math.floor(Math.random()*3);
+    // Choix de l'utilisateur
+    var userChoice = $('#userChoice option:selected').val();
+    //Si le choix de l'ordinateur et le même choix que celui de l'utilisateur alors :
+    if(computerChoice == userChoice){
+    //une boite d'alerte ce gènere (Egalité).
+      alert('égalité');
+      even++;
+    // Autrement si l'utilisateur gagner contre l'ordinateur exemple(Pierre > Ciseaux) alors :
+    }else if ((choices[userChoice] == 'Pierre' && choices[computerChoice] == 'Ciseaux') || (choices[userChoice] == 'Ciseaux' && choices[computerChoice] == 'Feuille') || (choices[userChoice] == 'Feuille' && choices[computerChoice] == 'Pierre')) {
+    // L'utilisateur gagne.
+      alert('Youpi, tu as gagné');
+      victory++;
+    //Sinon :
+    }else{
+    // L'ordinateur Gagne
+      alert('Youpi, tu as perdu');
+      defeat++;
+    }
+    var victoryPercentage = (victory/(victory + defeat + even))*100;
+    $('#result').html('<p class="resultVictory">Nombre de victoire : ' + victory + '(' + victoryPercentage +  '%)</p><p class="resultDefeat">Nombre de défaite : ' + defeat + '</p>');
+  });
 });
